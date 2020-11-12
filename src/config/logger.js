1f50a55b5392c51c6f32b07b4a8a4ed292cebfb1
jaidevmane <devmane.j@northeastern.edu>
2020-11-11 23:38:25 -0500

    adding logger to webapp

---
+++ src/config/logger.js
@@ -0,0 +1,19 @@
+const winston = require('winston');
+ 
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.simple(),
+  transports: [
+    //
+    // - Write all logs with level `error` and below to `error.log`
+    // - Write all logs with level `info` and below to `combined.log`
+    //
+    new winston.transports.File({ filename: 'csye6225.log' }),
+  ],
+});
+ 
+//
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+//
+module.exports = logger
