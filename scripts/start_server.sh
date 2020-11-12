ec80ad4d205bc13c33d4f9af5da46e1a5ef3b926
Jai Subash Devmane <69541788+jaidevmane@users.noreply.github.com>
2020-11-11 23:15:05 -0500

    adding metrics and logs to cloudwatch-config.json (#42)
    * adding cloudwatch to appspec hooks
    
    * adding metrics to cloudwatch-config.json

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 cd /home/ubuntu/webapp/
 
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+    -a fetch-config \
+    -m ec2 \
+    -c file:/opt/cloudwatch-config.json \
+    -s
+    
 sudo kill -9 $(sudo lsof -t -i:4000);
 sudo nohup npm start > /dev/null 2> /dev/null < /dev/null &
 
