5a1d181c89d7fab8111df2921bad4c49ebb7bf77
jaidevmane <devmane.j@northeastern.edu>
2020-11-12 00:38:50 -0500

    changing log location

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,7 +4,7 @@ cd /home/ubuntu/webapp/
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
     -a fetch-config \
     -m ec2 \
-    -c file:/opt/cloudwatch-config.json \
+    -c file:/home/ubuntu/webapp/cloudwatch-config.json \
     -s
     
 sudo kill -9 $(sudo lsof -t -i:4000);
