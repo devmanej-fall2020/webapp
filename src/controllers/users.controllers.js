86e59965eaccebdd75ee6d7be1b68128071fa871
Jai Subash Devmane <69541788+jaidevmane@users.noreply.github.com>
2020-12-02 05:56:10 -0500

    logger object stringify (#62)
    * sns test
    
    * adding logger to sns
    
    * logger sns bugs
    
    * topic arn updates
    
    * adding email to answer question
    
    * logging for bugs
    
    * logger object

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -1041,7 +1041,7 @@ exports.answerQuestion = async (req,res)=>{
                         data = JSON.parse(data);
                         // delete data.password;
                         console.log((data));
-                        logger.info(data);
+                        logger.info(JSON.stringify(data));
 
                         
                         var message = {
@@ -1052,16 +1052,6 @@ exports.answerQuestion = async (req,res)=>{
                             link: `http://www.api.prod.jaisubashdevmane.me/v1/${req.params.question_id}/answer`
                         };
                         
-
-                        sns_params.Message = JSON.stringify(message);
-
-                        message = {
-                            email_address : og_username,
-                            question_id: data.questionId,
-                            answer_id: data.id,
-                            answer_text: req.body.answer_text,
-                            link: `http://www.api.prod.jaisubashdevmane.me/v1/${req.params.question_id}/answer`
-                        }
 
                         sns_params.Message = JSON.stringify(message);
 
